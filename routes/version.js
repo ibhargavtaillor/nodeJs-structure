80d19630c93f0d800db80dd744315cf80cf18a71
Bhargav <bhargavt.spaceo@gmail.com>
2020-03-28 13:04:08 +0530

    Minor changes manage versioning

---
+++ routes/version.js
@@ -0,0 +1,14 @@
+/**
+ * @description Class contain all version routing
+ */
+class Version {
+
+    /**
+     * @description this function contain all version one routing
+     */
+    v1() {
+        
+    }
+}
+
+module.exports = new Version();
