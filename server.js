94e80a7e98c6fe9b45dd786b0302bda748789d1b
Bhargav <bhargavt.spaceo@gmail.com>
2020-07-05 19:21:56 +0530

    Initial Commit

--- server.js
+++ server.js
@@ -3,12 +3,20 @@ require("dotenv").config({
     "path": ".env"
 });
 
-var mysql = require("./config/database");
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 var express = require("express");
 var app = express();
 var routingVersion = require("./routes/version");
 
+/**
+ * Load default welcome page
+ */
 app.get("/", (_req, _res) => {
     _res.sendFile(__dirname + "/view/index.html");
 });
@@ -16,5 +24,5 @@ app.get("/", (_req, _res) => {
 app.use(express.static(__dirname + '/public'));
 
 app.listen(process.env.PORT, () => {
-    console.log("Welcome to nodejs easy structure and your server is runing on " + process.env.PORT);
+    console.log("Welcome to nodejs easy structure. Your server is runing on " + process.env.PORT);
 })
